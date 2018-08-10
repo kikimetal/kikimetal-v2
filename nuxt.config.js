f67483d3bea8dbff299acfba0f8b62b70371d903
kikimetal <reezafratzitz2@gmail.com>
2018-08-10 18:41:27 +0900

    Nuxt テンプレート

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'kikimetal-v2',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'kikimetal.com ver2 with Nuxt' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#dd408b' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    /**
+    * add 2018.8.9. kikimetal
+    * IE11 のためのポリフィル
+    */
+    babel: {
+      presets: [
+        [
+          'vue-app',
+          {
+            targets:  { ie: 11 },
+            useBuiltIns: true
+          }
+        ]
+      ]
+    },
+    vendor: [ 'babel-polyfill' ],
+  },
+
+  css: [
+    'assets/common.scss'
+  ],
+}
